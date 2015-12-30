651732e97759d5dcd4f093be7fc3362810d194b7
Angelo Biaggi <biagaany@gmail.com>
2015-12-29 20:01:41 -0500

    Javascript to parse basic data from xml and populate the html with very little css

--- Ticker_Main/Ticker_scripts.js
+++ Ticker_Main/Ticker_scripts.js
@@ -1,87 +1,74 @@
 ﻿window.onload = function () {
 
-    document.getElementsByClassName("ClickHere").onclick = function loadDoc() {
+    document.getElementById("ClickHere").onclick = function loadDoc() {
 
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
-                myFunction(xhttp);
+                main_function(xhttp);
             }
         };
         xhttp.open("GET", "NFL_scores_1.xml", true);
         xhttp.send();
+    }
 
+    function main_function(xml) {
 
-        function myFunction(xml) {
+        //h=home, a=away
+        var i, y;
+        var day, time, quarter, hTeam, hMascot, aTeam, aMascot;
+        var hscore, aScore, redZone, week, year;
+        var xmlDoc = xml.responseXML;
 
-            //h=home, a=away
-            var i;
-            var day, time, quarter, hTeam, hMascot, aTeam, aMascot;
-            var hscore, aScore, redZone, week, year;
-            var xmlDoc = xml.responseXML;
-            //var table = "<tr><th>Artist</th><th>Title</th></tr>";
+        x = xmlDoc.getElementsByTagName("gms");
+        txt = "";
 
-            x = xmlDoc.getElementsByTagName("gms");
-            txt = "";
+        var week = x[0].getAttribute('w');
+        var year = x[0].getAttribute('y');
 
-            var week = x[0].getAttribute('w');
-            var year = x[0].getAttribute('y');
+        x = xmlDoc.getElementsByTagName("g");
 
-            x = xmlDoc.getElementsByTagName("g");
+        //Parse the data for each game
+        for (i = 0; i < x.length; i++)
+        {
+            //Will have to check if data is there before we check
+            day = x[i].getAttribute('d');
+            time = x[i].getAttribute('t');
+            quarter = x[i].getAttribute('q');
+            hTeam = x[i].getAttribute('h');
+            hMascot = x[i].getAttribute('hnn');
+            hscore = x[i].getAttribute('hs');
+            aTeam = x[i].getAttribute('v');
+            aMascot = x[i].getAttribute('vnn');
+            aScore = x[i].getAttribute('vs');
+            redZone = x[i].getAttribute('rz');
 
-            for (i = 0; i < x.length; i++) {
-                day = x[i].getAttribute('d');
-                time = x[i].getAttribute('t');
-                quarter = x[i].getAttribute('q');
-                hTeam = x[i].getAttribute('h');
-                hMascot = x[i].getAttribute('hnn');
-                hscore = x[i].getAttribute('hs');
-                aTeam = x[i].getAttribute('v');
-                aMascot = x[i].getAttribute('vnn');
-                aScore = x[i].getAttribute('vs');
-                redZone = x[i].getAttribute('rz');
+            //create main game box
+            var divElement = document.createElement('div');
+            divElement.setAttribute("id", "game-box");
+            divElement.setAttribute("class", "game_css");
 
-                document.getElementsByClassName("game-opp").innerHTML = hTeam;
-                document.getElementsByClassName("score-opp").innerHTML = hscore;
-                document.getElementsByClassName("game-at").innerHTML = aTeam;
-                document.getElementsByClassName("score-at").innerHTML = aScore;
-            }
+            divElement.appendChild(makeDivForData(day, "game-day"));
+            divElement.appendChild(makeDivForData(time, "game-time"));
+            divElement.appendChild(makeDivForData(quarter, "game-qtr"));
+            divElement.appendChild(makeDivForData(hTeam, "home-team"));
+            divElement.appendChild(makeDivForData(hMascot, "home-mascot"));
+            divElement.appendChild(makeDivForData(hscore, "home-score"));
+            divElement.appendChild(makeDivForData(aTeam, "away-team"));
+            divElement.appendChild(makeDivForData(aMascot, "away-mascot"));
+            divElement.appendChild(makeDivForData(aScore, "game-day"));
+
+            document.getElementById("league").appendChild(divElement);
         }
     }
 
-    //function myFunction(xml) {
+    function makeDivForData(data, idName)
+    {
+        var div = document.createElement('div');
+        var divText = document.createTextNode(data);
+        div.setAttribute("id", idName);
+        div.appendChild(divText);
 
-    //    //h=home, a=away
-    //    var i;
-    //    var day, time, quarter, hTeam, hMascot, aTeam, aMascot;
-    //    var hscore, aScore, redZone, week, year;
-    //    var xmlDoc = xml.responseXML;
-    //    //var table = "<tr><th>Artist</th><th>Title</th></tr>";
-
-    //    x = xmlDoc.getElementsByTagName("gms");
-    //    txt = "";
-
-    //    var week = x[0].getAttribute('w');
-    //    var year = x[0].getAttribute('y');
-
-    //    x = xmlDoc.getElementsByTagName("g");
-
-    //    for (i = 0; i < x.length; i++) {
-    //        day = x[i].getAttribute('d');
-    //        time = x[i].getAttribute('t');
-    //        quarter = x[i].getAttribute('q');
-    //        hTeam = x[i].getAttribute('h');
-    //        hMascot = x[i].getAttribute('hnn');
-    //        hscore = x[i].getAttribute('hs');
-    //        aTeam = x[i].getAttribute('v');
-    //        aMascot = x[i].getAttribute('vnn');
-    //        aScore = x[i].getAttribute('vs');
-    //        redZone = x[i].getAttribute('rz');
-
-    //        document.getElementsByClassName("game-opp").innerHTML = hTeam;
-    //        document.getElementsByClassName("score-opp").innerHTML = hscore;
-    //        document.getElementsByClassName("game-at").innerHTML = aTeam;
-    //        document.getElementsByClassName("score-at").innerHTML = aScore;
-    //    }
-    //}
+        return div;
+    }
 }
