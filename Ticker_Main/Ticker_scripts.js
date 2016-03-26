63be04da4f9ff7057fc15167cf99d18b115ea0c1
Angelo Biaggi <biagaany@gmail.com>
2016-03-26 17:07:27 -0400

    Script to read and parse JSON file from reddit

--- Ticker_Main/Ticker_scripts.js
+++ Ticker_Main/Ticker_scripts.js
@@ -1,1 +1,29 @@
-﻿
+﻿$(document).ready(function () {
+        $.getJSON(
+        "http://www.reddit.com/r/news.json?jsonp=?",
+        function foo(data) {
+
+            var numChildren = data.data.children.length;
+
+            $.each(
+              data.data.children.slice(0, numChildren + 1),
+              function (i, post) {
+                  var urlLink = post.data.url;
+                  urlLink = urlLink.replace(/\/$/, '');
+
+                  $("#news-ticker").append('<div id="news-post" class="news__post"> <a href=' + urlLink + '>'+ post.data.title + '</a> </div>');
+                  //$("#news-post").append('<br id="post-title">' + post.data.title + '</br>');
+                  //$("#reddit-content").append('<br>' + post.data.url);
+                  //$("#reddit-content").append('<br>' + post.data.permalink);
+                  //$("#reddit-content").append('<br>' + post.data.ups);
+                  //$("#reddit-content").append('<br>' + post.data.downs);
+                  //$("#reddit-content").append('<hr>');
+              }
+            )
+        }
+      )
+      //.success(function () { alert("second success"); })
+      //.error(function () { alert("error"); })
+      //.complete(function () { alert("complete"); })
+
+});
