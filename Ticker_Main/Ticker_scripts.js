d2dea17aa32ac847245fa161e1e1d18e9a822add
Angelo Biaggi <biagaany@gmail.com>
2016-05-15 20:24:46 -0400

    Text scrolling with correct duration synamically set

--- Ticker_Main/Ticker_scripts.js
+++ Ticker_Main/Ticker_scripts.js
@@ -4,12 +4,14 @@
         function foo(data) {
 
             var numChildren = data.data.children.length;
+            var dataSize = 0;
 
             $.each(
               data.data.children.slice(0, numChildren + 1),
               function (i, post) {
                   var urlLink = post.data.url;
                   urlLink = urlLink.replace(/\/$/, '');
+                  dataSize += post.data.title.length;
 
                   $("#news-ticker").append('<div id="news-post" class="news__post"> <a href=' + urlLink + '>'+ post.data.title + '</a> &middot </div>');
                   //$("#news-post").append('<br id="post-title">' + post.data.title + '</br>');
@@ -20,6 +22,11 @@
                   //$("#reddit-content").append('<hr>');
               }
             )
+
+            var time = dataSize / 12;
+            var duration = time + 's';
+
+            $("#news-ticker").children("*").css("-webkit-animation-duration", duration);
         }
       )
       //.success(function () { alert("second success"); })
