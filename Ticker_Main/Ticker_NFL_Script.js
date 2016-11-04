30c44fe705a9cd23bb457b9684fdd0d98aff9cdb
Angelo Biaggi <biagaany@gmail.com>
2016-11-03 20:50:56 -0400

    This has soem nfl stuff in it

--- Ticker_Main/Ticker_NFL_Script.js
+++ Ticker_Main/Ticker_NFL_Script.js
@@ -3,15 +3,27 @@
     "http://www.nfl.com/liveupdate/scorestrip/ss.json",
     function foo(data) {
 
-        var numChildren = data.gms.length;
+        var numGames = data.gms.length;
         var dataSize = 0;
 
         $.each(
-          data.data.children.slice(0, numChildren + 1),
+          data.gms.slice(0, numGames + 1),
           function (i, post) {
               //var urlLink = post.data.url;
               //urlLink = urlLink.replace(/\/$/, '');
               //dataSize += post.data.title.length;
+              var homeTeam = post.h;
+              var visTeam = post.v;
+              var homeScore = post.hs;
+              var visScore = post.vs;
+
+              $("#nfl-ticker").append('<div id="game-box" class="game_box"></div>');
+              $("#game-box").append('<div id="team-info" class="team_info"></div>');
+              $("#team-info").append('<div id="home-team" class="home_team"></div>');
+              $("#team-info").append('<div id="visitor-team" class="visitor_team"></div>');
+              $("#team-info").append('<div id="home-score" class="home_score"></div>');
+              $("#team-info").append('<div id="visitor-score" class="visitor_score"></div>');
+              //$("#game-box").append('<div id="time-info" class="time_info"></div>');
 
               //$("#news-ticker").append('<div id="news-post" class="news__post"> <a href=' + urlLink + '>' + post.data.title + '</a> &middot </div>');
               //$("#news-post").append('<br id="post-title">' + post.data.title + '</br>');
